972b5e29fe4541bc88d4b52bc3cca3e06725a401
Justine Cesar L. Ocampo <justinecesarocampo@gmail.com>
2026-06-20 20:43:49 +0800

    Add site config constants module
    Centralize resort name, contact phone, and social media URLs that
    were previously hardcoded in multiple places.

---
+++ app/utils/siteConfig.ts
@@ -0,0 +1,6 @@
+export const SITE_CONFIG = {
+  RESORT_NAME: "Kampo Ibayo",
+  CONTACT_PHONE: "+63 966 281 5123",
+  CONTACT_PHONE_HREF: "tel:+639662815123",
+  FACEBOOK_URL: "https://www.facebook.com/profile.php?id=61562942638753",
+} as const;
